6e30c27a73cb1cf7586968ac16f2c3f491688272
Maurya <surajaaa26746@gmail.com>
2021-09-26 12:16:06 +0530

    first commit

--- src/components/UploadRecord.js
+++ src/components/UploadRecord.js
@@ -1,22 +1,37 @@
-import React from 'react'
-import { useSelector, useDispatch } from 'react-redux';
+import React, {useState, useEffect} from 'react'
+import { useSelector, useDispatch} from 'react-redux';
 import {fileUpload} from '../actions/AccountStatementAction'
 
 function UploadRecord() {
+      const [item, setitem] = useState("")
+      const [fileDate,setfileData]=useState("")
       const dispatch = useDispatch()
-      const myState = useSelector((store)=> store.accountStatementReducer )
+      const myState = useSelector((store)=> store.accountStatementReducer)
+      localStorage.setItem("FileData",JSON.stringify(myState.FileData))
+      const cartItems=(localStorage.getItem("FileData"))
+      useEffect(()=>{
+            setfileData(cartItems)
+      })
 
+    
+      console.log("fileDate",fileDate)
       console.log(myState.FileData)
-      // const [file, setfile] = useState("")
-      var files,formData;
+     
+     var files
       const setfile=(e)=>{
             console.log(e.target.files)
             files = e.target.files
-            formData = new FormData();
-            formData.append('text_file',files)
-            console.log(formData)}
-
+      }
+      
       const hangelFile=()=>{
+            // const  dataURL = canvas.toDataURL(files[0]).toString();
+            // localStorage.setItem((files[0].name, dataURL);
+            const reader = new FileReader()
+            reader.addEventListener("load",()=>{
+                  localStorage.setItem(files[0].name, reader.result);
+            })
+            reader.readAsDataURL(files[0])
+            
             const d = new Date();
             const date = d.getDate()+ "/" + d.getMonth() + "/" + d.getFullYear() +" "+ d.getHours() +":"+d.getMinutes() 
             const object ={
@@ -26,14 +41,48 @@ function UploadRecord() {
             }
             dispatch(fileUpload(object))
       }
-
+       
+      const Download =()=>{
+            myState.FileData.map((Element)=>{
+            const {name}=Element
+            const data = localStorage.getItem(name)
+            return setitem(data)
+      })
+      }
       
       return (
+           
             <div>
                 <h1>Upload file in React</h1>
                 <input type="file" onChange={ (e)=>setfile(e)} name='text_file'></input> 
                 <button type="button" onClick={hangelFile}>Upload</button>
+                <div>
+                <table className="table table-striped">
+                     
+                     <tr>
+                     <th>#</th>
+                     <th>Username</th>
+                     <th>Date</th>
+                     <th>Download</th>
+                     </tr>
+                     <tbody>
+                           {   
+                             fileDate && JSON.parse(fileDate).map((Element)=>{
+                                       const {id,name,date} = Element
+                                       return (
+                                             <tr><td>{id}</td>
+                                             <td>{name}</td>
+                                             <td>{date}</td>
+                                             <td><a href={item} download={name} onClick={Download}><button type="button">Download</button> </a></td></tr>
+                                       )
+                                 })
+                           }
+                          
+                        </tbody>
+                        </table>
+                </div>
             </div>
+
       )
 }
 
